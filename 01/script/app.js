481c21b7fe23d6e1d85364eefad48e27f8a15ca1
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-05-27 14:35:57 +0500

    bug js

---
+++ 01/script/app.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+
+function showError(username, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'
+}
+
+
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+
+    if (username.value === '') {
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
+
+
+})

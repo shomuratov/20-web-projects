f1d737d3341fe6bc14df7440f6c5461b168c6340
Azamat Shomuratov <106393064+shomuratov@users.noreply.github.com>
2023-05-29 11:56:58 +0500

    end

--- 01/script/app.js
+++ 01/script/app.js
@@ -1,27 +1,59 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const password2 = document.getElementById('password2')
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
+// showError function 
 
-function showError(username, message) {
+function showError(input, message) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control error'
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message; 
+};
+
+// Success message
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
 
+// Email valid 
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase())
+  };
 
 
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-
-
     if (username.value === '') {
         showError(username, 'Username is required');
     } else {
         showSuccess(username);
     }
 
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!validateEmail(email.value)) {
+        showError(email, 'Email is not valid');
+    }  else {
+        showSuccess(email);
+    }
 
-})
+    if (password.value === '') {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Confirm password is required');
+    } else {
+        showSuccess(password2);
+    }
+});
